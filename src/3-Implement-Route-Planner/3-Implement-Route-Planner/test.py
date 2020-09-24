f6d4dd760838550bf3ebd1f04f4f78041fc9ced7
Paulo Lopez <pau_lo_lop_ez@protonmail.com>
2020-09-24 13:05:22 -0700

    Update test.py

--- src/3-Implement-Route-Planner/3-Implement-Route-Planner/test.py
+++ src/3-Implement-Route-Planner/3-Implement-Route-Planner/test.py
@@ -6,6 +6,7 @@ MAP_40_ANSWERS = [
     (8, 24, [8, 14, 16, 37, 12, 17, 10, 24])
 ]
 
+
 def test(shortest_path_function):
     map_40 = load_map('map-40.pickle')
     correct = 0
@@ -14,7 +15,7 @@ def test(shortest_path_function):
         if path == answer_path:
             correct += 1
         else:
-            print("For start:", start, 
+            print("For start:", start,
                   "Goal:     ", goal,
                   "Your path:", path,
                   "Correct:  ", answer_path)
@@ -22,4 +23,3 @@ def test(shortest_path_function):
         print("All tests pass! Congratulations!")
     else:
         print("You passed", correct, "/", len(MAP_40_ANSWERS), "test cases")
-    
