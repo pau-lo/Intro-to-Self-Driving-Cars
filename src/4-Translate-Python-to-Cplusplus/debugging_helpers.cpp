d6f0150462a4720c26ac4f91ad55b2277ad8fc0c
Paulo Lopez <pau_lo_lop_ez@protonmail.com>
2020-09-24 13:10:08 -0700

    Update debugging_helpers.cpp

--- src/4-Translate-Python-to-Cplusplus/debugging_helpers.cpp
+++ src/4-Translate-Python-to-Cplusplus/debugging_helpers.cpp
@@ -15,39 +15,41 @@ using namespace std;
     	   vectors of floats) which will usually
     	   represent a robot's beliefs.
 */
+
 void show_grid(vector<vector<float>> grid)
 {
-	int i, j;
-	float p;
-	vector<float> row;
-	for (i = 0; i < grid.size(); i++)
-	{
-		row = grid[i];
-		for (j = 0; j < row.size(); j++)
-		{
-			p = row[j];
-			cout << p << ' ';
-		}
-		cout << endl;
-	}
+    int i, j;
+    float p;
+    vector<float> row;
+    for (i = 0; i < grid.size(); i++)
+    {
+        row = grid[i];
+        for (j = 0; j < row.size(); j++)
+        {
+            p = row[j];
+            cout << p << ' ';
+        }
+        cout << endl;
+    }
 }
 
 /**
     Displays a grid map of the world
 */
+
 void show_grid(vector<vector<char>> map)
 {
-	int i, j;
-	char p;
-	vector<char> row;
-	for (i = 0; i < map.size(); i++)
-	{
-		row = map[i];
-		for (j = 0; j < row.size(); j++)
-		{
-			p = row[j];
-			cout << p << ' ';
-		}
-		cout << endl;
-	}
+    int i, j;
+    char p;
+    vector<char> row;
+    for (i = 0; i < map.size(); i++)
+    {
+        row = map[i];
+        for (j = 0; j < row.size(); j++)
+        {
+            p = row[j];
+            cout << p << ' ';
+        }
+        cout << endl;
+    }
 }
